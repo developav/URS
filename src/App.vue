d91a2fa5f3353d3fd114e6e29e80b60c866320e7
developav <joker89sn@icloud.com>
2023-03-01 21:21:06 +0300

    first commit1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="header-container">
+      <a class="header-logo" href="#"></a>
+      <div class="header-helper">
+        <span></span>
+        <button class="header__button">UPGRADE</button>
+        <span></span>
+      </div>
+    </div>
+    
+  </header>
+</template>
